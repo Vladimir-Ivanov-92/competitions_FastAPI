d5f3765d1b6fc8167aacb2d9c3a10af3af4c356b
vova2092 <vovai2092@yandex.ru>
2024-02-09 12:44:45 +0300

    Update test_tournaments.py with black

--- tests/test_tournaments.py
+++ tests/test_tournaments.py
@@ -6,29 +6,23 @@ tournament_1_create = {
     "tournament": {
         "datetime": "2023-01-01T12:00:00",
         "sport_id": 1,
-        "name": "Tornament_1"
+        "name": "Tornament_1",
     },
     "athletes_with_place": [
-        {
-            "athlete_id": 1,
-            "place": 2
-        },
-        {
-            "athlete_id": 2,
-            "place": 1
-        }
-    ]
+        {"athlete_id": 1, "place": 2},
+        {"athlete_id": 2, "place": 1},
+    ],
 }
 
 
 async def test_create_tournament_handler(async_client: AsyncClient):
-    """ Успешное добавление данных по турниру """
+    """Успешное добавление данных по турниру"""
 
     response_data = {
-        'datetime': '2023-01-01T12:00:00',
-        'id': 1,
-        'name': 'Tornament_1',
-        'sport_id': 1
+        "datetime": "2023-01-01T12:00:00",
+        "id": 1,
+        "name": "Tornament_1",
+        "sport_id": 1,
     }
 
     # Добавляем спорт rowing в БД
@@ -43,36 +37,36 @@ async def test_create_tournament_handler(async_client: AsyncClient):
 
 
 async def test_create_tournament_handler_fail(async_client: AsyncClient):
-    """ Повторное добавление турнира, уже существующего в БД """
+    """Повторное добавление турнира, уже существующего в БД"""
 
     response = await async_client.post("/tournaments/", json=tournament_1_create)
     assert response.status_code == 400
 
 
 async def test_get_tournaments_handler(async_client: AsyncClient):
-    """ Получение данных всех турниров в БД """
+    """Получение данных всех турниров в БД"""
 
     response_data = [
         {
-            'datetime': '2023-01-01T12:00:00',
-            'id': 1,
-            'name': 'Tornament_1',
-            'sport_id': 'rowing',
-            'athletes': [
+            "datetime": "2023-01-01T12:00:00",
+            "id": 1,
+            "name": "Tornament_1",
+            "sport_id": "rowing",
+            "athletes": [
                 {
-                    'country': 'Russia',
-                    'first_name': 'Kirill',
-                    'id': 2,
-                    'last_name': 'kirillov',
-                    'place': 1
+                    "country": "Russia",
+                    "first_name": "Kirill",
+                    "id": 2,
+                    "last_name": "kirillov",
+                    "place": 1,
                 },
                 {
-                    'country': 'Russia',
-                    'first_name': 'Ivan',
-                    'id': 1,
-                    'last_name': 'Ivanov',
-                    'place': 2
-                }
+                    "country": "Russia",
+                    "first_name": "Ivan",
+                    "id": 1,
+                    "last_name": "Ivanov",
+                    "place": 2,
+                },
             ],
         }
     ]
